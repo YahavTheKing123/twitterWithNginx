f907f4efa81fad17eac1efdb43269addf5dcf76d
Adi <adi@khafla.com>
2021-08-04 19:25:59 +0300

    TwitterPocClient

---
+++ TwitterPocClient/src/app/home/home.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../_services/auth.service';
+import { TokenStorageService } from '../_services/token-storage.service';
+import { UserService } from '../_services/user.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  content?: string;
+
+  constructor(private userService: UserService, private router:Router, private tokenStorage: TokenStorageService) { }
+
+  ngOnInit(): void {
+
+    var user = this.tokenStorage.getUser();
+    if (!user)
+    {
+       this.router.navigate(['/register']);
+    }
+    //
+    /*
+    this.userService.getPublicContent().subscribe(
+      data => {
+        this.content = data;
+      },
+      err => {
+        this.content = JSON.parse(err.error).message;
+      }
+    );
+    */
+  }
+}
