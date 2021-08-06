2f2d310888c793c3b61aa9689c4ac59881683828
Adi <adi@khafla.com>
2021-08-06 19:45:10 +0300

    TwitterPocClient - route to global feed and my feed

--- TwitterPocClient/src/app/app-routing.module.ts
+++ TwitterPocClient/src/app/app-routing.module.ts
@@ -4,11 +4,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { GlobalFeedComponent } from './global-feed/global-feed.component';
+import { MyFeedComponent } from './my-feed/my-feed.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'globalFeed', component: GlobalFeedComponent },
+  { path: 'myFeed', component: MyFeedComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
